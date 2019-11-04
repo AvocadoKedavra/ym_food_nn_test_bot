9a9bcdefbb851894e2c5672275cf8c212f541d67
developer9000 <obluchkov@gmail.com>
2019-11-04 14:17:43 -0400

    Added users.json for logging users. Added new logic for botMenuMessage

--- handyFunctions.js
+++ handyFunctions.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+
 module.exports = {
     botMenuMessage: function(shopName, shopMenu, ctx){
         console.log('Clicked! on ' + shopName);
-        console.log(ctx.message.chat.id);
+        console.log(ctx.from);
+        var json = JSON.stringify(ctx.from) + ',\n';
+        fs.appendFile("users.json", json , function (err) {
+            if (err) throw err;
+            console.log('The "data to append" was appended to file!');
+         });
+        if (shopMenu == '') {
+            return ctx.replyWithMarkdown('Ресторан - или убрал страницу меню, или в корне изменил форматирование страницы. Как только я пойму что с ней - я всё поправлю. \n_- @alamamala_');
+        }
         return ctx.replyWithMarkdown('_'+ shopName +':_' + shopMenu);
     },
     remove_spaces: function(err, str) {
